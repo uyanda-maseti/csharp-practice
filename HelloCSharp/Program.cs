84790fb7ca19a298fa918a2d6ad88cbbca134c48
Uyanda <128975995+uyanda-maseti@users.noreply.github.com>
2026-02-10 11:31:24 +0000

    write som more console outputs

--- HelloCSharp/Program.cs
+++ HelloCSharp/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("The current time is " + DateTime.Now);
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
--- HelloCSharp/obj/Debug/net10.0/HelloCSharp.AssemblyInfo.cs
+++ HelloCSharp/obj/Debug/net10.0/HelloCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e6f4310e59b921494c97a92f9ca0f5ec1ad012")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+519a2225e7672df6cca1880440ba27009dfd4982")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
